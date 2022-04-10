d78639943e7e7908dbb4d6b6d2cea8cd06410a87
Tao135 <1295072844@qq.com>
2022-04-10 23:06:08 +0800

    @Tao135 Mybatis-Plus与Spring Boot整合 注意事项：导入坐标时，需注意版本号各个是否对应； 启动类需要放在包下而不是再java下； 导入注解包时需注意是否正确。

---
+++ czt-mybatis-plus-spring/src/main/java/cn/czt/mp/simple/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package cn.czt.mp.simple.mapper;
+
+import cn.czt.mp.simple.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ czt-mybatis-springboot/src/main/java/cn/czt/mp/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package cn.czt.mp.mapper;
+
+import cn.czt.mp.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
