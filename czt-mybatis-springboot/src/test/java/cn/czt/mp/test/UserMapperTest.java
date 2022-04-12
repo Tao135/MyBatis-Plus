0985de9ca8a5c30da1271607339e886efabc62fc
Tao135 <1295072844@qq.com>
2022-04-12 16:33:16 +0800

    @Tao135 通用CRUD之更新操作 操作步骤： 1.根据Id进行修改时，可以直接封装在user实体中，使用updateById(user)进行更新； 2.根据条件进行修改时，可用实体user封装更新内容，使用QueryWrapper<User>.eq("a","b")进行条件判定，判定条件为a=b的用户数据 3.根据条件进行修改时，可用UpdateWrapper<User>.set("","").set("","")封装修改数据，.eq("a","b")进行条件判定。

--- czt-mybatis-springboot/src/test/java/cn/czt/mp/test/UserMapperTest.java
+++ czt-mybatis-springboot/src/test/java/cn/czt/mp/test/UserMapperTest.java
@@ -2,6 +2,8 @@ package cn.czt.mp.test;
 
 import cn.czt.mp.mapper.UserMapper;
 import cn.czt.mp.pojo.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +41,38 @@ public class UserMapperTest {
         for (User user : userList) {
             System.out.println(user);
         }
+    }
+
+    @Test
+    public void test3(){
+        User user = new User();
+        user.setId(1L);
+        user.setPassword("558");
+        user.setAge(18);
+        int result = this.userMapper.updateById(user);
+        System.out.println("result => " + result);
+    }
+
+    @Test
+    public void test4(){
+        User user = new User();
+        user.setAge(20);    //更新的字段
+        user.setPassword("888888");
+
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_name","zhangsan");        //匹配user_name = zhangsan 的用户数据
+
+        int result = this.userMapper.update(user, wrapper);
+        System.out.println("result => " + result);
+    }
+
+    @Test
+    public void test5(){
+        UpdateWrapper<User> wrapper = new UpdateWrapper<>();
+        wrapper.set("age",21).set("password","666666")  //更新的字段
+        .eq("user_name","zhangsan");         //更新的条件
+
+        int result = this.userMapper.update(null, wrapper);
+        System.out.println("result => " + result);
     }
 }
