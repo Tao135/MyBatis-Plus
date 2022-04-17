7a7eb9e273571f5193ba938da0174b810a4f8ea2
Tao135 <1295072844@qq.com>
2022-04-17 13:41:56 +0800

    @Tao135 ActiveRecord用法 1.需要在实体类中继承Model<实体类> 2.mapper文件不能删除 3.可以直接使用自带的方法

---
+++ czt-mybatis-springboot/src/test/java/cn/czt/mp/test/TestUserMapper2.java
@@ -0,0 +1,68 @@
+package cn.czt.mp.test;
+
+import cn.czt.mp.pojo.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUserMapper2 {
+
+    @Test
+    public void testSelectById(){
+        User user = new User();
+        user.setId(1L);
+
+        User user1 = user.selectById(user);
+        System.out.println(user1);
+    }
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setUserName("tao");
+        user.setPassword("1234");
+        user.setAge(22);
+        user.setName("Tao");
+        user.setMail("tao@123");
+
+        boolean insert = user.insert();
+        System.out.println("result =>" + insert);
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setId(5L);         //查询条件
+        user.setName("哈哈");     //更新的数据
+
+        boolean b = user.updateById();
+        System.out.println(b);
+    }
+
+    @Test
+    public void testDelete(){
+        User user = new User();
+        user.setId(7L);
+
+        boolean b = user.deleteById();
+        System.out.println(b);
+    }
+
+    @Test
+    public void testSelect(){
+        User user = new User();
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.ge("age",30);
+
+        List<User> users = user.selectList(wrapper);
+        for (User user1 : users) {
+            System.out.println(user1);
+        }
+    }
+}
