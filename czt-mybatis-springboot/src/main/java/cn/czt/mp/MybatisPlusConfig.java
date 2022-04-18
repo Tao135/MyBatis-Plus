41af5e270eaa23f651fa7d1e8715e777b618f76e
Tao135 <1295072844@qq.com>
2022-04-18 21:44:00 +0800

    @Tao135 Mybatis-Plus插件 拦截器  作用：当执行某种sql语句时，进行拦截

--- czt-mybatis-springboot/src/main/java/cn/czt/mp/MybatisPlusConfig.java
+++ czt-mybatis-springboot/src/main/java/cn/czt/mp/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package cn.czt.mp;
 
+import cn.czt.mp.plugins.MyInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,10 @@ public class MybatisPlusConfig {
     @Bean           //配置分页插件
     public PaginationInterceptor paginationInterceptor(){
         return new PaginationInterceptor();
+    }
+
+    @Bean           //自定义拦截器
+    public MyInterceptor myInterceptor(){
+        return new MyInterceptor();
     }
 }
